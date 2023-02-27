5677e41cbf4f66cde3794f0d3eea29d6ab5c8981
Spoorti-Gandhad <109356770+Spoorti-Gandhad@users.noreply.github.com>
2023-02-27 13:42:57 +0530

    download attr

--- excelStyle_v21.js
+++ excelStyle_v21.js
@@ -109,7 +109,8 @@ looker.plugins.visualizations.add({
       console.log(downloadUrl); // Prints the download URL to the console
       //sleep(1000);
       //window.open(downloadUrl);
-      window.open(downloadUrl, "_blank");
+      //window.open(downloadUrl, "_blank");
+      window.open(downloadUrl, 'Download');
     });
   },
   
