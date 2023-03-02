a21e3ac62a5ed3b3979836116bb195a04dfde851
Spoorti-Gandhad <109356770+Spoorti-Gandhad@users.noreply.github.com>
2023-03-02 12:58:37 +0530

    Update and rename excelStyle_v36.js to tableToExcel.js

--- tableToExcel.js
+++ tableToExcel.js
@@ -66,17 +66,19 @@ looker.plugins.visualizations.add({
     downloadButton.innerHTML = 'Download as Excel';
     downloadButton.className = 'download-button';
     downloadButton.addEventListener('click', (event) => {
-         var uri = 'data:application/vnd.ms-excel;base64,'
-            , template = '<html xmlns:o="urn:schemas-microsoft-com:office:office" xmlns:x="urn:schemas-microsoft-com:office:excel" xmlns="http://www.w3.org/TR/REC-html40"><head><!--[if gte mso 9]><xml><x:ExcelWorkbook><x:ExcelWorksheets><x:ExcelWorksheet><x:Name>{worksheet}</x:Name><x:WorksheetOptions><x:DisplayGridlines/></x:WorksheetOptions></x:ExcelWorksheet></x:ExcelWorksheets></x:ExcelWorkbook></xml><![endif]--></head><body><table>{table}</table></body></html>'
-            , base64 = function(s) { return window.btoa(unescape(encodeURIComponent(s))) }
-            , format = function(s, c) { return s.replace(/{(\w+)}/g, function(m, p) { return c[p]; }) }
-          return function() {
-            if (!table.nodeType) table_xls = document.getElementById("looker_table")
-            var ctx = {worksheet: '26' || 'Worksheet', table: table_xls.innerHTML}
-            window.location.href = uri + base64(format(template, ctx))
-          }
+      var uri = 'data:application/vnd.ms-excel;base64,'
+        , template = '<html xmlns:o="urn:schemas-microsoft-com:office:office" xmlns:x="urn:schemas-microsoft-com:office:excel" xmlns="http://www.w3.org/TR/REC-html40"><head><!--[if gte mso 9]><xml><x:ExcelWorkbook><x:ExcelWorksheets><x:ExcelWorksheet><x:Name>{Worksheet}</x:Name><x:WorksheetOptions><x:DisplayGridlines/></x:WorksheetOptions></x:ExcelWorksheet></x:ExcelWorksheets></x:ExcelWorkbook></xml><![endif]--><meta http-equiv="content-type" content="text/plain; charset=UTF-8"/></head><body><table>{table}</table></body></html>'
+        , base64 = function (s) { return window.btoa(unescape(encodeURIComponent(s))) }
+        , format = function (s, c) {
+          const regex = /style="([^"]*)"/g;
+          return s.replace(/{(\w+)}/g, function (m, p) {
+            const cellHtml = c[p];
+            const cellHtmlWithStyle = cellHtml.replace(regex, function (m, p1) {
+              return 'style="' + p1 + '"';
+            });
+            return cellHtmlWithStyle;
           });
-        },
+        };
       var table = document.querySelector('table');
       table.style.border = '1px solid black';
       table.style.fontSize = '11px';
@@ -139,7 +141,7 @@ looker.plugins.visualizations.add({
     var generatedHTML = `
       <style>
         .table {
-          font-size: 11pt;
+          font-size: ${config.font_size}px;
           border: 1px solid black;
           border-collapse: collapse;
           margin:auto;
@@ -150,7 +152,7 @@ looker.plugins.visualizations.add({
           border-collapse: collapse;
           font-weight: normal;
           font-family: 'Verdana';
-          font-size: 11pt;
+          font-size: 11px;
           align-items: center;
           text-align: center;
           margin: auto;
@@ -164,7 +166,7 @@ looker.plugins.visualizations.add({
           border-collapse: collapse;
           font-weight: normal;
           font-family: 'Verdana';
-          font-size: 11pt;
+          font-size: 11px;
           align-items: center;
           text-align: center;
           margin: auto;
