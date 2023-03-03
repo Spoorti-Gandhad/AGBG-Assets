614f5b90516db5f97e3e16cb3e68c70514fc0af6
Spoorti-Gandhad <109356770+Spoorti-Gandhad@users.noreply.github.com>
2023-03-03 13:18:11 +0530

    Update and rename tableToExcel_v4.js to tableToExcel_v5.js

--- tableToExcel_v5.js
+++ tableToExcel_v5.js
@@ -67,10 +67,29 @@ looker.plugins.visualizations.add({
     downloadButton.className = 'download-button';    
     downloadButton.addEventListener('click', (event) => {
       var uri = 'data:application/vnd.ms-excel;base64,'
-        , template = '<html xmlns:o="urn:schemas-microsoft-com:office:office" xmlns:x="urn:schemas-microsoft-com:office:excel" xmlns="http://www.w3.org/TR/REC-html40"><head><!--[if gte mso 9]><xml><x:ExcelWorkbook><x:ExcelWorksheets><x:ExcelWorksheet><x:Name>{worksheet}</x:Name><x:WorksheetOptions><x:DisplayGridlines/></x:WorksheetOptions></x:ExcelWorksheet></x:ExcelWorksheets></x:ExcelWorkbook></xml><![endif]--></head><body><table>{table}</table></body></html>'
-        , base64 = function(s) { return window.btoa(unescape(encodeURIComponent(s))) }
-        , format = function(s, c) { return s.replace(/{(\w+)}/g, function(m, p) { return c[p]; }) };
-      var table = document.querySelector('table');
+       // , template = '<html xmlns:o="urn:schemas-microsoft-com:office:office" xmlns:x="urn:schemas-microsoft-com:office:excel" xmlns="http://www.w3.org/TR/REC-html40"><head><!--[if gte mso 9]><xml><x:ExcelWorkbook><x:ExcelWorksheets><x:ExcelWorksheet><x:Name>{worksheet}</x:Name><x:WorksheetOptions><x:DisplayGridlines/></x:WorksheetOptions></x:ExcelWorksheet></x:ExcelWorksheets></x:ExcelWorkbook></xml><![endif]--></head><body><table>{table}</table></body></html>'
+         , template = '<html xmlns:o="urn:schemas-microsoft-com:office:office" xmlns:x="urn:schemas-microsoft-com:office:excel" xmlns="http://www.w3.org/TR/REC-html40"><head><!--[if gte mso 9]><xml><x:ExcelWorkbook><x:ExcelWorksheets><x:ExcelWorksheet><x:Name>{worksheet}</x:Name><x:WorksheetOptions><x:DisplayGridlines/></x:WorksheetOptions></x:ExcelWorksheet></x:ExcelWorksheets></x:ExcelWorkbook></xml><![endif]--></head><body><table>{table}</table></body></html>',
+        //, base64 = function(s) { return window.btoa(unescape(encodeURIComponent(s))) }
+          ,base64 = function(s) {
+            return window.btoa(unescape(encodeURIComponent(s)))
+          },
+        //, format = function(s, c) { return s.replace(/{(\w+)}/g, function(m, p) { return c[p]; }) };
+          ,format = function(s, c) {
+            return s.replace(/{(\w+)}/g, function(m, p) {
+              return c[p];
+            })
+          }
+      var toExcel = document.getElementById("toExcel").innerHTML;
+      var ctx = {
+        worksheet: name || '',
+        table: toExcel
+      };
+      var link = document.createElement("a");
+      link.download = "export.xls";
+      link.href = uri + base64(format(template, ctx))
+      link.click();
+      
+      /*var table = document.querySelector('table');
       table.style.border = '1px solid black';
       table.style.fontSize = '11px';
       var rows = table.rows;
@@ -91,9 +110,10 @@ looker.plugins.visualizations.add({
           cell.setAttribute('style', style);
         }
       }
-      const XLSX = document.createElement('script');
-      XLSX.src = 'https://cdn.jsdelivr.net/npm/xlsx/dist/xlsx.full.min.js';
-      document.head.appendChild(XLSX);
+      const scr = document.createElement('script');
+      scr.src = 'https://cdn.jsdelivr.net/npm/xlsx/dist/xlsx.full.min.js';
+      //scr.src = "https://unpkg.com/xlsx@0.15.1/dist/xlsx.full.min.js";
+      document.head.appendChild(scr);
       var ctx = { Worksheet: '26', table: table.innerHTML }
       var xl = format(template, ctx);
       const downloadUrl = uri + base64(xl);
@@ -108,7 +128,7 @@ looker.plugins.visualizations.add({
        //document.body.appendChild(link);
        //link.click();
        //document.body.removeChild(link);
-       //delete link;
+       //delete link;*/
     });
   },
   
