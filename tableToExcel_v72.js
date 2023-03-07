c2767181b429af73c1495965391d12f32562fef6
Spoorti-Gandhad <109356770+Spoorti-Gandhad@users.noreply.github.com>
2023-03-07 14:51:36 +0530

    Update and rename tableToExcel_v71.js to tableToExcel_v72.js

--- tableToExcel_v72.js
+++ tableToExcel_v72.js
@@ -68,7 +68,7 @@ looker.plugins.visualizations.add({
     downloadButton.setAttribute('height', '25px');
     downloadButton.setAttribute('width', '25px');
     downloadButton.setAttribute('title', 'Download As Excel');
-    downloadButton.style.marginLeft='85%';
+    downloadButton.style.marginLeft='92%';
     //downloadButton.className = 'download-button';   
     this._container.prepend(downloadButton);
     downloadButton.addEventListener('click', (event) => {
