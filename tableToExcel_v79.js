3a0b948be78c6dcdfbe2f6e7a0e9b5cdea4f3e48
Spoorti-Gandhad <109356770+Spoorti-Gandhad@users.noreply.github.com>
2023-03-13 15:27:37 +0530

    Update and rename tableToExcel_v78.js to tableToExcel_v79.js

--- tableToExcel_v79.js
+++ tableToExcel_v79.js
@@ -75,7 +75,7 @@ looker.plugins.visualizations.add({
           var downloadLink;
           var dataType = 'application/vnd.ms-excel';
           var tableContent = document.getElementById('htmltable');
-          var tableHTMLdata = tableContent[0];
+          var tableHTMLdata = tableContent.innerHTML;
             //, template = '<html xmlns:o="urn:schemas-microsoft-com:office:office" xmlns:x="urn:schemas-microsoft-com:office:excel" xmlns="http://www.w3.org/TR/REC-html40"><head><!--[if gte mso 9]><xml><x:ExcelWorkbook><x:ExcelWorksheets><x:ExcelWorksheet><x:Name>{Worksheet}</x:Name><x:WorksheetOptions><x:DisplayGridlines/></x:WorksheetOptions></x:ExcelWorksheet></x:ExcelWorksheets></x:ExcelWorkbook></xml><![endif]--><meta http-equiv="content-type" content="text/plain; charset=UTF-8"/></head><body><table>{table}</table></body></html>'
             //, base64 = function (s) { return window.btoa(unescape(encodeURIComponent(s))) }
             //, format = function (s, c) {
