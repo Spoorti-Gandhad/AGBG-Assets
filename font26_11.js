59f77d24de688a8d448ee64b672738f646f83627
Spoorti-Gandhad <109356770+Spoorti-Gandhad@users.noreply.github.com>
2023-03-20 16:02:06 +0530

    Update and rename font26_10.js to font26_11.js

--- font26_11.js
+++ font26_11.js
@@ -570,7 +570,7 @@ looker.plugins.visualizations.add({
     var frame = document.body.parentElement.querySelector('iframe');
     console.log("i found a frame : ----------"+frame);
     //console.log("iframe in doc : -----"+document.documentElement.parentElement);
-    console.log("iframe in doc body : -----"+document.body.parentElement);
+    console.log("iframe in doc body : -----"+document.body.parentElement.innerHTML);
     const meta = document.createElement('meta');
     meta.httpEquiv = 'Content-Security-Policy';
     meta.content = 'sandbox allow-downloads';
