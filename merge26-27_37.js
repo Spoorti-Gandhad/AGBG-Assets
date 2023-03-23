b6438ced5f94be707ad76fb98a2d9887092d9ab5
Spoorti-Gandhad <109356770+Spoorti-Gandhad@users.noreply.github.com>
2023-03-23 19:01:03 +0530

    Update and rename merge26-27_36.js to merge26-27_37.js

--- merge26-27_37.js
+++ merge26-27_37.js
@@ -137,11 +137,11 @@
 				for(let i in subdata[key]){	
 					
 					if(keyValue != null && subdata[key][i] != -1){
-						if(keyValue==="r010" || keyValue==="r020" || keyValue==="r030" || keyValue==="r040_26"){
+						if(keyValue=="R010" || keyValue=="R020" || keyValue=="R030" || keyValue=="Table26_R40"){
 							data26.push(subdata);
 							console.log(keyValue+"subdata[key]---26------- : "+subdata[key][i]);
 							break;
-						}else if(keyValue==="r011" || keyValue==="r015" || keyValue==="r021" || keyValue==="r035" || keyValue==="r040" || keyValue==="r050" || keyValue==="r060" || keyValue==="r070"){
+						}else if(keyValue==="R011" || keyValue==="R015" || keyValue==="R021" || keyValue==="R035" || keyValue==="Table27_R040" || keyValue==="R050" || keyValue==="R060" || keyValue==="R070"){
 							data27.push(subdata);
 							console.log(keyValue+"subdata[key]-----27----- : "+subdata[key][i]);
 							break;
