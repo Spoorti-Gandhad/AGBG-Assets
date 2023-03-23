e2fb8689e92e219b1326181c9b1f33591f25529c
Spoorti-Gandhad <109356770+Spoorti-Gandhad@users.noreply.github.com>
2023-03-23 16:36:06 +0530

    Update and rename merge26-27_24.js to merge26-27_25.js

--- merge26-27_25.js
+++ merge26-27_25.js
@@ -133,14 +133,16 @@
 		  for(subdata of data){
 			for(let key in subdata){
 				var keyValue = key.split(".")[1];
-				
+				for(let i in subdata[key]){
+					console.log("........................."+i);
+				}
 				if(keyValue != null){
 					if(keyValue==="r010" || keyValue==="r020" || keyValue==="r030" || keyValue==="r040_26"){
 						data26.push(subdata);
-						console.log(keyValue+"----------26 sb data key value ---------"+subdata[key]);
+						//console.log(keyValue+"----------26 sb data key value ---------"+subdata[key]);
 					}else if(keyValue==="r011" || keyValue==="r015" || keyValue==="r021" || keyValue==="r035" || keyValue==="r040" || keyValue==="r050" || keyValue==="r060" || keyValue==="r070"){
 						data27.push(subdata);
-						console.log(keyValue+"----------27 sb data key value ---------"+subdata[key]);
+						//console.log(keyValue+"----------27 sb data key value ---------"+subdata[key]);
 					}
 				}
 			}
@@ -153,7 +155,7 @@
 			
 			// Next columns are the data
 			for (row of data26) {
-			  //console.log("row : ---------- "+row[field.name]);
+			  console.log("row : ---------- "+row[field.name]);
 			  if(row[field.name]!== null ){
 				  generatedHTML += `<td class='table-cell'>${LookerCharts.Utils.htmlForCell(row[field.name])}</td>`
 			  }
@@ -250,8 +252,6 @@
 	 
 		 
 		generatedHTML27 += "</table>"; 
-		this._container.innerHTML = generatedHTML+generatedHTML27;
-
 		done();
 	  }
 	});
